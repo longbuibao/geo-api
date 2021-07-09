b9ae55b78ec9d8e3ae2ba097b44c33d1a773e68e
Linh Phạm <18520992@gm.uit.edu.vn>
2021-07-09 22:53:27 +0700

    add polygon

--- router/home.js
+++ router/home.js
@@ -3,13 +3,15 @@ const router = express.Router()
 const geoJSON = require('geojson');
 
 const { getAllPatientAndPoint } = require('../controllers/routers/ui/get-all-patient-point')
+const { getAllPolygon } = require('../controllers/cachli/cachli.controller')
 
 router.get('/home', async(req, res) => {
     try {
         const patients = await getAllPatientAndPoint()
-
+        const polygons = await getAllPolygon()
         res.render('home', {
             patients,
+            polygons,
             title: "Trang chủ"
         })
     } catch (error) {
